5eef8f26a78da3cba9169a47e8856345c098ca48
Zkeq <learnonly@learnonly.xyz>
2022-03-28 07:16:09 +0800

    v1

--- v1/player.js
+++ v1/player.js
@@ -1,18 +1,18 @@
 
+
 function playM3u8(url){
-    if(Hls.isSupported()) {
-        var video = document.getElementById('player');
-        video.volume = 1.0;
-        var hls = new Hls();
-        console.log(url)
-        video.source = url
-        hls.loadSource(m3u8Url);
-        hls.attachMedia(video);
-        hls.on(Hls.Events.MANIFEST_PARSED,function() {
-          video.play();
-        });
-        document.title = url
-      }
-  }
-  
-  playM3u8(window.location.href.split("url=")[1])
+  if(Hls.isSupported()) {
+      var video = document.getElementById('video');
+      video.volume = 1.0;
+      var hls = new Hls();
+      var m3u8Url = decodeURIComponent(url)
+      hls.loadSource(m3u8Url);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED,function() {
+        video.click().play();
+      });
+      document.title = url
+    }
+}
+
+playM3u8(window.location.href.split("url=")[1])
